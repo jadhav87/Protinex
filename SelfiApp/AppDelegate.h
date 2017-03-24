cb3d5196a22e77fc36758864924ca87a5dd3069e
vikas <vikas@vikass-iMac.local>
2017-03-24 10:35:17 +0530

    Initial Commit

---
+++ SelfiApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelfiApp
+//
+//  Created by vikas on 24/03/17.
+//  Copyright © 2017 vikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelfiApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelfiApp
+//
+//  Created by vikas on 24/03/17.
+//  Copyright © 2017 vikas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
